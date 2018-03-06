a0a8c7365d1dca0f626da8dd991a2d4c57139833
drngsl <drngsl@qq.com>
2018-03-06 16:38:39 +0800

    Add requirements

--- blockchain.py
+++ blockchain.py
@@ -58,12 +58,12 @@ class BlockChain(object):
 
     def new_transaction(self, sender, recipient, amount):
         """
-            生成新交易信息，信息将加入到下一个待挖的区块中
-            :param sender: <str> Address of the Sender
-            :param recipient: <str> Address of the Recipient
-            :param amount: <int> Amount
-            :return: <int> The index of the Block that will hold this transaction
-            """
+        生成新交易信息，信息将加入到下一个待挖的区块中
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
